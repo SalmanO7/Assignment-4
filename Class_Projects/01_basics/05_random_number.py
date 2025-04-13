b8cddae7c9e2217c2d4f7ac6576d8dc8e997e1ea
SalmanO7 <msalman221020@gmail.com>
2025-04-14 01:59:38 +0500

    complete random number

---
+++ Class_Projects/01_basics/05_random_number.py
@@ -0,0 +1,11 @@
+import random
+
+
+def main():
+    for i in range(10):
+        new = random.randint(1,100)
+        print(new, end= " ")
+    print()    
+
+if __name__ == '__main__':
+    main()        
